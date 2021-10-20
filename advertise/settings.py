36a7da59687eac03f74adf52ac8c51e5be46e3bd
Tejaswi-Chaudhari <tejschaudhari522002@gmail.com>
2021-10-20 14:17:14 +0530

    code dump

--- advertise/settings.py
+++ advertise/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -72,6 +73,8 @@ TEMPLATES = [
         },
     },
 ]
+
+DEBUG = True
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
@@ -136,6 +139,9 @@ MEDIA_DIR = os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
